522fa1163b484bba840e258f487a3b80846aca54
Robin <robin.gambin@gmail.com>
2016-10-03 18:55:45 -0400

    Started recode

--- Halloween_2016/Halloween_2016.ino
+++ Halloween_2016/Halloween_2016.ino
@@ -1,31 +1,69 @@
 /*
- * Halloween 2016 Music and Lights for Skull Head
+ * Halloween 2016 Music, SFX and Lights for Skull Head
  * Robin and Finley Gambin
  */
 
-#include <SD.h>
-#include <SPI.h>
-#include <MusicPlayer.h>
 #include <arduino.h> //not sure if I need this one or if it is there by default
 
 const int eyes = 2; //dio for eyes
-const int head = 14; //dio for servo
-char song = "Halloween.mp3";
+const int head = 3; //dio for servo
+const int music = 4; //dio for music
+const int sfx1 = 5; //dio sound fx 1
+const int sfx2 = 6; //dio sound fx 2
+const int sfx3 = 7; //dio sound fx 2
+const int detect1 = 8; //dio for detect sensor 1
+const int detect2 = 9; //dio for detect sensor 2
+const char a = "LOW";
+const char b = "HIGH";
 
-
-void setup() {
+void setup() 
+{
   // put your setup code here, to run once:
   Serial.begin(9600);
-  player.begin();
-  player.playOne(song);
   pinMode(eyes,OUTPUT);
   pinMode(head,OUTPUT); 
+  pinMode(music,OUTPUT);
+  pinMode(sfx1,OUTPUT);
+  pinMode(sfx2,OUTPUT);
+  pinMode(sfx3,OUTPUT);
+  pinMode(detect1,INPUT);
+  pinMode(detect2,INPUT);
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  player.play();
-  digitalWrite(eyes,HIGH);
-  delay(250);
-  digitalWrite(eyes,LOW);
+void loop() 
+{
+ if(digitalRead(detect1) == HIGH)
+{
+ // move head right and flash lights code here. Probably add for loop.
+ digitalWrite(eyes,b);
+ digitalWrite(sfx1,a);
+ digitalWrite(head,b);
 }
+ else if (digitalRead(detect2)==HIGH)
+{
+void head_left();
+}
+ else 
+{
+//Just play music file here
+digitalWrite(music,a);
+}
+}
+
+
+void head_left(void)
+{
+int i=0;
+//move head left and flash lights code here
+ digitalWrite(sfx2,a);
+ digitalWrite(head,a);
+ for (i=0;i>=15;i++)
+{
+ digitalWrite(eyes,b);
+ delay(100);
+ digitalWrite(eyes,a);
+}
+ digitalWrite(head,a);
+}
+
