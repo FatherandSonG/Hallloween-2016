5a33fc8bfc29e53551ebd3f95934d66a126474ae
Robin <robin.gambin@gmail.com>
2016-10-04 08:43:22 -0400

    updated function
    Updated function and added commented code

--- Halloween_2016/Halloween_2016.ino
+++ Halloween_2016/Halloween_2016.ino
@@ -1,4 +1,4 @@
-/*
+u/*
  * Halloween 2016 Music, SFX and Lights for Skull Head
  * Robin and Finley Gambin
  */
@@ -13,13 +13,13 @@ const int sfx2 = 6; //dio sound fx 2
 const int sfx3 = 7; //dio sound fx 2
 const int detect1 = 8; //dio for detect sensor 1
 const int detect2 = 9; //dio for detect sensor 2
-const char a = "LOW";
-const char b = "HIGH";
+const char l = "LOW"; //state control
+const char h = "HIGH"; //state control
 
 void setup() 
 {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  // Setup ports
+  Serial.begin(9600); //remove this later?
   pinMode(eyes,OUTPUT);
   pinMode(head,OUTPUT); 
   pinMode(music,OUTPUT);
@@ -33,22 +33,20 @@ void setup()
 
 void loop() 
 {
- if(digitalRead(detect1) == HIGH)
-{
- // move head right and flash lights code here. Probably add for loop.
- digitalWrite(eyes,b);
- digitalWrite(sfx1,a);
- digitalWrite(head,b);
-}
- else if (digitalRead(detect2)==HIGH)
-{
-void head_left();
-}
- else 
-{
-//Just play music file here
-digitalWrite(music,a);
-}
+  if(digitalRead(detect1) == HIGH)
+  {
+		digitalWrite(music,h); // turn off music
+    void head_left();  //call head movemnent and SFX fucntion
+  }
+  else if (digitalRead(detect2)==HIGH)
+  {
+    digitalWrite(music,h);
+		void head_left();
+  }
+  else 
+  {
+    digitalWrite(music,a); //Play music if no motion detected
+  }
 }
 
 
@@ -56,14 +54,16 @@ void head_left(void)
 {
 int i=0;
 //move head left and flash lights code here
- digitalWrite(sfx2,a);
- digitalWrite(head,a);
- for (i=0;i>=15;i++)
-{
- digitalWrite(eyes,b);
- delay(100);
- digitalWrite(eyes,a);
-}
- digitalWrite(head,a);
+  digitalWrite(sfx2,a);
+  digitalWrite(head,a);
+  for (i=0;i>=15;i++)
+  {
+    digitalWrite(eyes,h);
+    delay(250);
+    digitalWrite(eyes,l);
+    delay(250);
+  }
+	digitalWrite(eyes,l); //reset eyes
+  digitalWrite(head,l); //reset head
 }
 
