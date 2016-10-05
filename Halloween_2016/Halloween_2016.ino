8340c69d321a4adace2915ff96953283fdad22c8
Robin <robin.gambin@gmail.com>
2016-10-05 17:57:01 -0400

    Major code review and circuit test
    Actually tested this code with a breadboard.

--- Halloween_2016/Halloween_2016.ino
+++ Halloween_2016/Halloween_2016.ino
@@ -1,69 +1,76 @@
-u/*
+/*
  * Halloween 2016 Music, SFX and Lights for Skull Head
  * Robin and Finley Gambin
  */
 
 #include <arduino.h> //not sure if I need this one or if it is there by default
 
-const int eyes = 2; //dio for eyes
-const int head = 3; //dio for servo
-const int music = 4; //dio for music
-const int sfx1 = 5; //dio sound fx 1
-const int sfx2 = 6; //dio sound fx 2
-const int sfx3 = 7; //dio sound fx 2
-const int detect1 = 8; //dio for detect sensor 1
-const int detect2 = 9; //dio for detect sensor 2
-const char l = "LOW"; //state control
-const char h = "HIGH"; //state control
+int eyes = 2; //dio for eyes
+int headlight1 = 3; //dio for headlight
+int music = 4; //dio for music
+int sfx1 = 5; //dio sound fx 1
+int sfx2 = 6; //dio sound fx 2
+int detect1 = 7; //dio for detect sensor 1
+int detect2 = 8; //dio for detect sensor 2
+//int head = 11; //dio for head
+int time1;
+int time2;
+int time3;
 
 void setup() 
 {
   // Setup ports
   Serial.begin(9600); //remove this later?
   pinMode(eyes,OUTPUT);
-  pinMode(head,OUTPUT); 
+  pinMode(headlight1,OUTPUT);
   pinMode(music,OUTPUT);
   pinMode(sfx1,OUTPUT);
   pinMode(sfx2,OUTPUT);
-  pinMode(sfx3,OUTPUT);
   pinMode(detect1,INPUT);
   pinMode(detect2,INPUT);
-  
-}
+  }
 
 void loop() 
 {
-  if(digitalRead(detect1) == HIGH)
+ if(digitalRead(detect1) == HIGH)
   {
-		digitalWrite(music,h); // turn off music
-    void head_left();  //call head movemnent and SFX fucntion
+  	digitalWrite(music,HIGH); // turn off music
+    head_left();  //call head movemnent and SFX fucntion
   }
   else if (digitalRead(detect2)==HIGH)
   {
-    digitalWrite(music,h);
-		void head_left();
+    digitalWrite(music,HIGH);
+		head_left();
   }
   else 
   {
-    digitalWrite(music,a); //Play music if no motion detected
+    digitalWrite(music,LOW); //Play music if no motion detected
+    digitalWrite(eyes,LOW); //just to make sure they are off
+    digitalWrite(headlight1,LOW); //just to make sure they are off
+    
   }
 }
 
 
-void head_left(void)
+void head_left()
 {
 int i=0;
-//move head left and flash lights code here
-  digitalWrite(sfx2,a);
-  digitalWrite(head,a);
-  for (i=0;i>=15;i++)
+//move head left and flash lights code here. 4 seconds total
+  time1=millis();
+  digitalWrite(sfx1,LOW);
+  digitalWrite(headlight1,HIGH);
+  
+  for (i=0;i<=15;i++)
   {
-    digitalWrite(eyes,h);
-    delay(250);
-    digitalWrite(eyes,l);
-    delay(250);
+    digitalWrite(eyes,HIGH);
+    delay(150);
+    digitalWrite(eyes,LOW);
+    delay(100);
   }
-	digitalWrite(eyes,l); //reset eyes
-  digitalWrite(head,l); //reset head
+	digitalWrite(eyes,LOW); //reset eyes
+  digitalWrite(headlight1,LOW); //reset headlight
+  time2=millis();
+  time3=(time2-time1);
+  Serial.println(time3);
 }
 
