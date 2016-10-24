b7a48017399a14cafd92123df19844af033c33b6
Robin <robin.gambin@gmail.com>
2016-10-23 22:34:44 -0400

    Big update to Skeleton Head
    Fixed motor drive. Added sound fx and ultrasonic sensor code. Seems to
    be working great.

--- Halloween_2016/Halloween_2016.ino
+++ Halloween_2016/Halloween_2016.ino
@@ -4,6 +4,9 @@
  */
 
 #include <arduino.h> //not sure if I need this one or if it is there by default
+#include "PinChangeInterrupt.h"
+
+#define detect1 7
 
 int eyes = 3; //dio for eyes
 int headlight1 = 9; //dio for headlight
@@ -12,7 +15,7 @@ int coffin_white = 6;
 int music = 13; //dio for music
 int sfx1 = 12; //dio sound fx 1
 int sfx2 = 11; //dio sound fx 2
-int detect1 = 7; //dio for detect sensor 1
+//int detect1 = 7; //dio for detect sensor 1
 int detect2 = 8; //dio for detect sensor 2
 int time1;
 int time2;
@@ -31,11 +34,12 @@ void setup()
   pinMode(sfx2,OUTPUT);
   pinMode(detect1,INPUT_PULLUP); //enable internal pullup detect is on active low
   pinMode(detect2,INPUT_PULLUP);
+  attachPCINT(digitalPinToPCINT(detect1), head_light, CHANGE);
 }
 
 void loop() 
 {
- coffin=random(0,255);
+ coffin=random(50,255); //used to make the coffin lights flicker
  
  if(digitalRead(detect1) == LOW)
   {
@@ -53,10 +57,11 @@ void loop()
     digitalWrite(sfx1,HIGH);
     digitalWrite(sfx2,HIGH);
     analogWrite(eyes,0); //just to make sure they are off
-    digitalWrite(headlight1,LOW); //just to make sure they are off
+    analogWrite(headlight1,0); //just to make sure they are off
     analogWrite(coffin_red,coffin);
     analogWrite(coffin_white,coffin);
-    //analogWrite(headlight1,100);  
+    //analogWrite(headlight1,100); 
+    delay(100); 
   }
 }
 
@@ -67,10 +72,10 @@ int i=0;
 //flash lights code here. 4 seconds total
   time1=millis();
   digitalWrite(sfx1,LOW);
-  digitalWrite(headlight1,HIGH);
+  analogWrite(headlight1,coffin);
    for (int a=0; a<=255;a++)               //loop from 0 to 255
   {
-    analogWrite(eyes, a);
+        analogWrite(eyes, a);
         delay(8);                             //wait for 8 milliseconds    
                
   }
@@ -79,12 +84,11 @@ int i=0;
     analogWrite(eyes, a);               // set the brightness of pin 9:
        delay(8);                             //wait for 8 milliseconds   
   }
- 	analogWrite(eyes,0); //reset eyes
-  digitalWrite(headlight1,LOW); //reset headlight
+ 	analogWrite(eyes,10); //reset eyes
   digitalWrite(sfx1,HIGH);
-  delay(200); //delay to reset head
+ // delay(200); //delay to reset head
   time2=millis();
   time3=(time2-time1);
-  Serial.println("LED:");
-  Serial.println(time3);
+  //Serial.println("LED:");
+ // Serial.println(time3);
 }
--- Skeleton_Head/Skeleton_Head.ino
+++ Skeleton_Head/Skeleton_Head.ino
@@ -1,9 +1,13 @@
 /*
- * Skeleton Head Movement 2016 MMovement for Skull Head
+ * Skeleton Head Movement and sound fxs
+ * Halloween 2016
+ * Code was written for Arduino UNO, Adafruit Sound FX Board 16MB 
+ * with HC-SR04 for trick or treater detection.
+ * Uses a generic stepper motor to drive the head.
  * Robin and Finley Gambin
  */
 
-#include <arduino.h> //not sure if I need this one or if it is there by default
+#include <arduino.h>
 #include <Stepper.h> //stepper motor library
 
 # define STEPS_PER_MOTOR_REV 32
@@ -11,32 +15,99 @@
 
 Stepper small_stepper(STEPS_PER_MOTOR_REV, 10,12,11,13);
 
-int Steps2Take=512;
-int photo_detect = 7; //dio for detect sensor 1
+int Steps2Take=400; //steps to move from center to look of shoulder
+int pingPin = 7; //trigger HC-SR04
+int echoPin = 8; //return HC-SR04
+const int a = 5000; //minimum distance to trigger
+int head; //result of ping
+int music=2; //play music
+int sfx1=3; //play sound fx 1
+int sfx2=4; //play sound fx 2
+int sfx3=5; //play sound fx 3
+int playing=6; //is sound still playing?
 
 void setup() 
 {
-  // Setup ports
- // Serial.begin(9600); //remove this later?
-  pinMode(photo_detect,INPUT_PULLUP);
+  // Setup ports and pins
+  Serial.begin(9600); //remove this later?
+  pinMode(pingPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(sfx1,OUTPUT);
+  pinMode(sfx2,OUTPUT);
+  pinMode(sfx3,OUTPUT);
+  pinMode(music,OUTPUT);
+  pinMode(playing,INPUT);
   }
 
 void loop() 
-{
- if(digitalRead(photo_detect) == HIGH)
+{ 
+  digitalWrite(sfx1,HIGH); //make sure sfx is off
+  digitalWrite(sfx2, HIGH);
+  digitalWrite(sfx3, HIGH);
+  digitalWrite(music,LOW); //play music
+  
+  ping(); //check distance
+
+ if((head) ==1)
   {
-  	head_move();
+  	head_move(); //move head to the right
   	}
    else 
   {
-    head_move();
+   delay(100); //delay before next ping
   }
 }
 
-void head_move()
+void head_move() //head move function
 {
-  small_stepper.setSpeed(700);   
-  small_stepper.step(Steps2Take);
-  Steps2Take = -Steps2Take;
+  int x=random(1,3); //pick random sound fx
+  digitalWrite(music, HIGH); //turn off music
+  small_stepper.setSpeed(500);   //move head to the right
+  small_stepper.step(300);
+  switch(x) //select sound fx based on random number x
+ {
+  case 1:
+  digitalWrite(sfx1, LOW); //turn on sfx trigger
+  delay(250); //wait for sound to start
+  digitalWrite(sfx1,HIGH); //turn off sfx trigger
+  break;
+  case 2:
+  digitalWrite(sfx2,LOW);
+  delay(250);
+ digitalWrite(sfx2,HIGH);
+  break;
+  case 3:
+  digitalWrite(sfx3,LOW);
+  delay(250);
+  digitalWrite(sfx3,HIGH);
+  break;
+ }
+ 
+do //wait for music to stop playing to reset head position
+  {
+   delay(100);
+  } while (digitalRead(playing) == LOW);
+  digitalWrite(music,LOW); //turn music on
+  small_stepper.setSpeed(500); //reset head to center   
+  small_stepper.step(-300);
+}
+
+void ping() //function to test distance
+{
+  long duration; //init variable for pulse duration
+  digitalWrite(pingPin, LOW); //verify ping pin low
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH); //set ping pin high
+  delayMicroseconds(8);
+  digitalWrite(pingPin, LOW); //set ping pin low
+  duration=pulseIn(echoPin, HIGH); //read echo pulse
+  if ((duration) < 5000) //if pulse duration is less than 5000 move head
+  {
+    head= 1;
+  }
+  else
+  {
+    head= 0;
+  }
 }
 
